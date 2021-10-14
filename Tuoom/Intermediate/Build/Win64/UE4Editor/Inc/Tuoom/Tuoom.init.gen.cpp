8e31948dd13f2be8abae825fdd5a205ca4d2f59b
woojooc <86417404+woojooc@users.noreply.github.com>
2021-10-14 16:59:50 +0900

    사운드 추가

--- Tuoom/Intermediate/Build/Win64/UE4Editor/Inc/Tuoom/Tuoom.init.gen.cpp
+++ Tuoom/Intermediate/Build/Win64/UE4Editor/Inc/Tuoom/Tuoom.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeTuoom_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x50D179B7,
-				0xFA5E86C1,
+				0x448F9BEF,
+				0x98C29088,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
